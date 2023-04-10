0e92118d927bf97a46268fe2aac6ac23457e4f8e
ReidMcLain <reidmm@gmail.com>
2023-04-10 15:29:05 -0500

    first API endpoint!

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+
+WebApplicationBuilder webApplicationBuilder = 
+    WebApplication.CreateBuilder(args);
+
+WebApplication webApplication =
+    webApplicationBuilder.Build();
+
+webApplication.MapGet("/api/home",
+    (Func<string>)(() => "Hey, Reid was here creating this API EndPoint woaaaah spoooky"));
+
+webApplication.Run();
